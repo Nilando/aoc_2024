d77e137558428c07e1872ad1d22f3568e7245bd4
Nilando <nilandschumacher@gmail.com>
2024-12-08 20:33:04 -0800

    day 7 refactor

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,86 +1,52 @@
-fn main() {
-    part1();
-    part2();
+fn is_valid_part1(target: usize, sum: usize, nums: &[usize]) -> bool {
+    if nums.is_empty() {
+        return sum == target;
+    }
+
+    if sum > target {
+        return false
+    }
+
+    let operand = nums.first().unwrap();
+
+    is_valid_part1(target, sum + operand, &nums[1..nums.len()])
+    || is_valid_part1(target, sum * operand, &nums[1..nums.len()])
 }
 
-fn part1() {
+fn is_valid_part2(target: usize, sum: usize, nums: &[usize]) -> bool {
+    if nums.is_empty() {
+        return sum == target;
+    }
+
+    if sum > target {
+        return false
+    }
+
+    let operand = nums.first().unwrap();
+
+    is_valid_part2(target, sum + operand, &nums[1..nums.len()])
+    || is_valid_part2(target, sum * operand, &nums[1..nums.len()])
+    || is_valid_part2(target, format!("{}{}", sum, operand).parse().unwrap(), &nums[1..nums.len()])
+}
+
+fn main() {
     let input = std::fs::read_to_string("./input.txt").unwrap();
-    let mut part1: u128 = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
 
     for line in input.lines() {
         let raw_equation: Vec<&str> = line.split(':').collect();
         let target: usize = raw_equation[0].parse().unwrap();
         let operands: Vec<usize> = raw_equation[1].trim().split(' ').map(|s| s.parse().unwrap()).collect();
 
-        for i in 0..2_usize.pow(operands.len() as u32 - 1) {
-            let mut sum = operands[0];
-
-            for (idx, operand) in operands.iter().skip(1).enumerate() {
-                if sum > target {
-                    break;
-                }
-
-                if (i & (1 << idx)) != 0 {
-                    sum += operand;
-                } else {
-                    sum *= operand;
-                }
-            }
-
-            if sum == target {
-                println!("target: {}", target);
-                println!("operands: {:?}", operands);
-                println!("i: {:0<8b}", i);
-                part1 += target as u128;
-                break;
-            }
+        if is_valid_part1(target, operands[0], &operands[1..operands.len()]) {
+            part1 += target;
+            part2 += target;
+        } else if is_valid_part2(target, operands[0], &operands[1..operands.len()]) {
+            part2 += target;
         }
     }
 
     println!("Part 1: {}", part1);
-}
-
-fn part2() {
-    let input = std::fs::read_to_string("./input.txt").unwrap();
-    let mut part2: u128 = 0;
-
-    for line in input.lines() {
-        let raw_equation: Vec<&str> = line.split(':').collect();
-        let target: usize = raw_equation[0].parse().unwrap();
-        let operands: Vec<usize> = raw_equation[1].trim().split(' ').map(|s| s.parse().unwrap()).collect();
-
-        for i in 0..3_usize.pow(operands.len() as u32 - 1) {
-            let mut sum = operands[0];
-            let mut x = i;
-
-            for operand in operands.iter().skip(1) {
-                if sum > target {
-                    break;
-                }
-
-                match x % 3 {
-                    0 => sum += operand,
-                    1 => sum *= operand,
-                    2 => {
-                        let mut a = sum.to_string(); 
-                        let b = operand.to_string();
-                        a.push_str(b.as_str());
-
-                        sum = a.parse().unwrap();
-                    },
-                    _ => panic!("bad"),
-                }
-
-                x -= x % 3;
-                x = x / 3;
-            }
-
-            if sum == target {
-                part2 += target as u128;
-                break;
-            }
-        }
-    }
-
     println!("Part 2: {}", part2);
 }
